8649c9484b477ec07da16210aa16666e2dce8672
Tapan Kumer Das <voboghure.dev@gmail.com>
2022-06-22 22:25:10 +0600

    Able to load sticky-block.js file with jquery dependency
    Signed-off-by: Tapan Kumer Das <voboghure.dev@gmail.com>

--- wpwing-sticky-block.php
+++ wpwing-sticky-block.php
@@ -6,8 +6,8 @@
  * Requires PHP:      7.0
  * Version:           1.0.0
  * Author:            The WordPress Contributors
- * License:           GPL-2.0-or-later
- * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * License:           GPL-3.0-or-later
+ * License URI:       https://www.gnu.org/licenses/gpl-3.0.html
  * Text Domain:       wpwing-sticky-block
  *
  * @package           create-block
@@ -23,3 +23,39 @@ function wpwing_sticky_block_init() {
 }
 
 add_action( 'init', 'wpwing_sticky_block_init' );
+
+/**
+ * Load custom javascript dependent on jquery
+ *
+ * @since 1.0.0
+ */
+function wpwing_sticky_block_frontend() {
+  wp_register_script(
+    'wpwing_sticky_block-js',
+    plugins_url( 'sticky-block.js', __FILE__ ),
+    ['jquery'],
+    "1.0.0",
+    true
+  );
+  wp_enqueue_script( 'wpwing_sticky_block-js' );
+}
+
+// add_action( 'wp_enqueue_scripts', 'wpwing_sticky_block_frontend' );
+
+/**
+ * For test and debug, log function to view any data in wp-content/debug.log
+ * uses: log_it($variable);
+ *
+ * @since 1.0.0
+ */
+if ( ! function_exists( 'log_it' ) ) {
+  function log_it( $message ) {
+    if ( WP_DEBUG === true ) {
+      if ( is_array( $message ) || is_object( $message ) ) {
+        error_log( "\r\n" . print_r( $message, true ) );
+      } else {
+        error_log( $message );
+      }
+    }
+  }
+}
